61ddc47d0cdbd80a8dc51e9c95c55d04b6fa653a
Andawu100 <andrew.chester22@ycdsbk12.ca>
2019-10-01 09:46:15 -0400

    practice

--- Working/ClassExamples/Questions 1.py
+++ Working/ClassExamples/Questions 1.py
@@ -1,2 +1,2 @@
-hours = int(input("How many hours? "))
-print("This is ", (hours//24), "amount of days.")
+hours = float(input("How many hours? "))
+print("This is ", (hours/24.0), "amount of days.")
---
+++ Working/LiveHacks/LiveHack1/practice1.py
@@ -0,0 +1,20 @@
+'''
+-------------------------------------------------------------------------------
+Name: practice1.py
+Purpose: Convert Fahrenheit to Celcius
+
+Author: Andrew Chester
+
+Created: date 01/10/2019
+-------------------------------------------------------------------------------
+'''
+
+
+# Find the current temperature in Fahrenheit
+Fahrenheit = float(input("What is the temperature in Fahrenheit?: "))
+
+# compute celsius from fahrenheit
+celcius = 5/9 * (Fahrenheit-32)
+
+# Output in celcius
+print("The temperature in celcius is: ", celcius)
