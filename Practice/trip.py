657e464ad77ed1ac2b138ac75de833fbc8d6ddd9
Andawu100 <andrew.chester22@ycdsbk12.ca>
2019-11-08 09:51:12 -0500

    lol

---
+++ Practice/trip.py
@@ -0,0 +1,19 @@
+"""
+*********************************************************
+filename: trip.py
+Name: Andrew Chester
+Purpose: Calculate where you get to go on vacation.
+Date: 11/8/19
+*********************************************************
+"""
+#Get marks, earnings
+marks = float(input("What mark did you get?: "))
+earnings = float(input("How much have you made?: "))
+
+#compute destination
+if earnings >= 500 and marks >= 80:
+    print("You are going to Europe.")
+elif marks >= 80
+    pring("You get to go to California"):
+else:
+    print("You get to stay home.")
